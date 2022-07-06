60f99e2f0d0b49662f68353519a0cbc63f43d5dc
ScotuzziJr <scotuzzijunior@gmail.com>
2022-07-06 18:47:28 -0300

    feat: deploy setup

--- babel/settings.py
+++ babel/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure--0x=ki-(k&s2ev44ekw19#08b_c*xp51r=ua4umrst%+xji(+g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://ms-babel.herokuapp.com/']
 
 
 # Application definition
@@ -128,3 +129,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_on_heroku.settings(locals())
